9e0b9d2895e2d3d03a87a955f1a0d2b06ce177f3
b8build48 <enomajoel8@gmail.com>
2025-09-04 17:04:09 +0100

    Add files via upload

---
+++ bless.js
@@ -0,0 +1,10 @@
+setTimeout(()=>{
+    document.querySelector(".loader").classList.add("fade-out");
+setTimeout(()=>{
+    document.querySelector(".loader").remove();
+    window.location.href="lodedd.html"
+},284);
+},1848);
+
+
+
